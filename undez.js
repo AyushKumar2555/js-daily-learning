07975dfea0f6ea0f36a2369780126de77a25bf19
Ayush kumar <124113566+AyushKumar2555@users.noreply.github.com>
2025-05-22 10:26:24 +0530

    Add files via upload

---
+++ undez.js
@@ -0,0 +1,23 @@
+// Comparison Operators
+let e = 20; e += 5;  // a = 25
+let f = 20; f -= 5;  // b = 15
+let g = 20; g *= 5;  // c = 100
+let h = 20; h /= 5;  // d = 4
+let i = 20; i %= 5;  // e = 0
+console.log(a, b, c, d, e);
+
+// PostIncement And Preincrement
+let a = 5;
+console.log(a++); // 5 (then a becomes 6)
+console.log(a);   // 6
+
+let b = 5;
+console.log(++b); // 6 (b becomes 6 first)
+
+// Post And Predecrement\
+let c = 10;
+console.log(c--); // 10 (then c becomes 9)
+console.log(c);   // 9
+
+let d = 10;
+console.log(--d); // 9 (d becomes 9 first)
